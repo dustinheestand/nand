40acacbb485841a5860c0d70882a838289fcf094
dustinheestand <heestand@post.harvard.edu>
2019-01-10 15:53:31 -0600

    first two projects

---
+++ 02/bla.js
@@ -0,0 +1,6 @@
+for (let i = 1; i < 16; i++) {
+  console.log(
+    `    HalfAdder(a=temp${i}, b=in[${i + 1}], sum=out[${i +
+      1}], carry=temp${i + 1});`
+  );
+}
---
+++ 03/a/bla.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 16; i++) {
+  console.log(`Bit(in=in[${i}], load=load, out=out[${i}]`);
+}
