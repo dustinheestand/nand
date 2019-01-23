027187c97825b066b2c615cef3c2c9308b7fb5fc
dustinheestand <heestand@post.harvard.edu>
2019-01-23 11:52:23 -0600

    finished mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+  @R1
+  D=M // D = Memory[R1]
+  @LOOP
+  D;JGT // If R1 is already positive, jump to doing the addition already
+
+//Otherwise, set both R0 and R1 to negative themselves
+(NEG)
+  @R1
+  M=-M
+  @R0
+  M=-M
+(LOOP)
+  @R1
+  D=M
+  M=M-1
+  @END 
+  D;JEQ //If R1 = 0 before decrementing, jump to end
+  @R0
+  D=M //Otherwise store data at R0 in M
+  @R2
+  M=M+D //Then add that data to the data already in R2
+  @LOOP
+  0;JMP //And loop back unconditionally - loop breaks above
+(END)
+  @END
+  0;JMP
